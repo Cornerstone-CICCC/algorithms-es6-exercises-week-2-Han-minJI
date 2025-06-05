41b9894f120a60f567cb1b3de751fa9f92548461
Minji Han <gksalswl159@naver.com>
2025-06-05 00:06:39 -0700

    Added Exes

--- ex18.js
+++ ex18.js
@@ -10,6 +10,7 @@ In Square Code, the spaces are removed from the english text and the characters 
     dwouldha
     vegivenu
     sroots
+    
 The square root of 54 (Math.sqrt(54)) is 7.3484692283495345. If we round this number up (Math.ceil(Math.sqrt(54))), we get 8. If we use that for the number of characters on each line (the number of columns), then our text will be close to a square shape.
 
 The message is then coded by reading down the columns going left to right. For example, the message above is coded as:
@@ -24,6 +25,34 @@ Create a function named squareCode that will receive a message, and return the s
 
 const squareCode = function (message) {
   // Put your solution here
+  const splitMsg = message.split('')
+  splitMsg.forEach((char, index) => {
+    if(char === ' '){
+      splitMsg.splice(index, 1)
+    }
+  });
+
+  const charLength = splitMsg.length
+  const colNum = Math.ceil(Math.sqrt(charLength))
+  let array = []
+
+  for(let i=0; i<charLength; i+=colNum){
+    array.push(splitMsg.slice(i, i+colNum))
+  }
+
+  let result = '';
+  for (let col = 0; col < colNum; col++) {
+    for (let row of array) {
+      if (col < row.length) {
+        result += row[col];
+      }
+    }
+    result += ' '; 
+  }
+
+  return result
+
+
 };
 
 console.log(squareCode("chill out")); // clu hlt io
--- ex19.js
+++ ex19.js
@@ -66,3 +66,60 @@ Instruction
 Create a function generateBoard which will return a nested array representing the board, containing the location of two queens.
 Create a function called queenThreat that will indicate whether or not the two queens are positioned so that they attack each other.
 */
+
+
+const generateBoard = function(whiteQueen, blackQueen){
+
+  // 생성 및 초기화화
+  const arrayBoard = new Array(8).fill(0).map(() => Array(8).fill(0))
+
+  const [whiteRow, whiteCol] = whiteQueen
+  const [blackRow, blackCol] = blackQueen
+
+  arrayBoard[whiteRow] [whiteCol] = 1
+  arrayBoard[blackRow] [blackCol] = 1
+
+  console.log(arrayBoard)
+
+  return arrayBoard
+}
+
+const queenThreat = function(generateBoard){
+  let positions = []
+
+  console.log(generateBoard)
+
+
+  for(let i=0; i< generateBoard.length; i++){
+    for(let j=0; j<generateBoard[i].length; j++){
+      if(generateBoard[i][j] === 1){
+        positions.push([i,j])
+      }
+    }
+  }
+
+  // destructing
+  const [p1, p2] = positions
+  const [row1, col1] = p1
+  const [row2, col2] = p2
+
+  if(row1 === row2 
+    || col1 === col2
+    || Math.abs(row1-row2) === Math.abs(col1-col2)) {
+      return true
+    }else{ return false}
+
+}
+
+let whiteQueen = [0, 5];
+let blackQueen = [5, 0];
+let generatedBoard = generateBoard(whiteQueen, blackQueen);
+console.log(generatedBoard);
+console.log(queenThreat(generatedBoard));
+
+
+// let whiteQueen = [0, 0];
+// let blackQueen = [5, 7];
+// let generatedBoard = generateBoard(whiteQueen, blackQueen);
+// console.log(generatedBoard);
+// console.log(queenThreat(generatedBoard));
